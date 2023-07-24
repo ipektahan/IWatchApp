6a941bbfdc54da8863ab21374afaa099880f47a8
tahan <tahan@IPEK>
2023-07-24 04:14:12 +0300

    Add project files.

---
+++ IWatchApp/Data/IWatchDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using IWatchApp.Models.Domain;
+
+namespace IWatchApp.Data
+{
+    public class IWatchDbContext : DbContext
+    {
+        public IWatchDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+
+        public DbSet<Item> MyProperty { get; set; }
+    }
+}
---
+++ IWatchApp/Models/Domain/Item.cs
@@ -0,0 +1,19 @@
+﻿namespace IWatchApp.Models.Domain
+{
+    public class Item
+    {
+        public Guid Id { get; set; }
+        public string Type { get; set; }
+
+        public string URL { get; set; }
+
+        public long Price { get; set; }
+        
+        public DateTime DateofStart { get; set; } 
+
+        public DateTime DateofEnd { get; set; }
+
+
+
+    }
+}
